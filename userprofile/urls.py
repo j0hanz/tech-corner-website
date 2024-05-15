99f729861f55638db176b143f7254c423c04f074
Linus Johansson <golfarenlj@gmail.com>
2024-05-15 18:22:50 +0000

    configure profile img to work when debug is True

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -5,3 +5,11 @@ urlpatterns = [
     path("edit-profile/", views.edit_profile, name="edit_profile"),
     path("profile/", views.profile_page, name="profile_page"),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
