dfec5017b2e9a46e97dbb0cfd917cf9acc32109a
Linus Johansson <golfarenlj@gmail.com>
2024-05-17 10:14:48 +0000

    updated url path for delete account

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import delete_account
 
 urlpatterns = [
     path("edit-profile/", views.edit_profile, name="edit_profile"),
     path("profile/", views.profile_page, name="profile_page"),
+    path('delete_account/', delete_account, name='delete_account'),
 ]
