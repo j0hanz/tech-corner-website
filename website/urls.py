e47ae7df740b32315be7e48bc4b122ca550b3497
Linus Johansson <golfarenlj@gmail.com>
2024-05-17 19:36:55 +0000

    added path for creating posts

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("posts/<slug:slug>/", views.post_detail, name="post_detail"),
+    path('create-post/', views.create_post, name='create_post'),
 ]
