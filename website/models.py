46b13c520dc740a7f8140cec6987c4fd8da2caa4
Linus Johansson <golfarenlj@gmail.com>
2024-05-17 19:36:35 +0000

    updated models for creating posts. Slug is now automated when post is done via website

--- website/models.py
+++ website/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
-# Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, blank=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="user_posts"
     )
-    banner = models.ImageField(default="logo.webp", blank=True)
+    banner = CloudinaryField("image", default="logo.webp", blank=True)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -23,3 +26,19 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
+
+
+def unique_slug(instance, new_slug=None):
+    slug = slugify(instance.title) if new_slug is None else new_slug
+    Klass = instance.__class__
+    qs_exists = Klass.objects.filter(slug=slug).exists()
+    if qs_exists:
+        new_slug = f"{slug}-{Klass.objects.filter(slug=slug).count() + 1}"
+        return unique_slug(instance, new_slug=new_slug)
+    return slug
+
+
+@receiver(pre_save, sender=Post)
+def save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug(instance)
