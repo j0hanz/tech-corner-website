69ec256810cfe6f3ca0b59e1f978a75494f61dc7
Linus Johansson <golfarenlj@gmail.com>
2024-05-17 19:37:16 +0000

    new function for creating posts

--- website/views.py
+++ website/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Post
+from .forms import PostForm
+
+
+@login_required
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("index")
+    else:
+        form = PostForm()
+    return render(request, "website/create_post.html", {"form": form})
 
 
 def index(request):
