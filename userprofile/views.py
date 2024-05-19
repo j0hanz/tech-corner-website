83205682caf0afb98efc3e37f622092b0b56bfee
Linus Johansson <golfarenlj@gmail.com>
2024-05-19 09:22:16 +0000

    new function that make users able to view their own posts

--- userprofile/views.py
+++ userprofile/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.decorators import login_required
 from .forms import UserProfileForm, UserForm
 from .models import UserProfile
 from django.contrib.auth.models import User
+from website.models import Post
 from django.contrib import messages
+
 
 @login_required
 def edit_profile(request):
@@ -35,6 +37,11 @@ def edit_profile(request):
     )
 
 
+def user_posts(request):
+    posts = Post.objects.filter(author=request.user)
+    return render(request, "userprofile/user_posts.html", {"posts": posts})
+
+
 @login_required
 def profile_page(request):
     user_profile, created = UserProfile.objects.get_or_create(
@@ -52,6 +59,8 @@ def delete_account(request):
     if request.method == "POST":
         user = request.user
         user.delete()
-        messages.success(request, 'Your account has been deleted successfully.')
-        return redirect('index')
-    return render(request, 'userprofile/delete_account_confirm.html')
+        messages.success(
+            request, "Your account has been deleted successfully."
+        )
+        return redirect("index")
+    return render(request, "userprofile/delete_account_confirm.html")
