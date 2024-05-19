0cb2a9e7b78ad9c09e8c33b856c0c2bc4d7a8e93
Linus Johansson <golfarenlj@gmail.com>
2024-05-19 09:22:41 +0000

    url path for new template user_posts

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("edit-profile/", views.edit_profile, name="edit_profile"),
     path("profile/", views.profile_page, name="profile_page"),
     path('delete_account/', delete_account, name='delete_account'),
+    path('user-posts/', views.user_posts, name='user_posts'),
 ]
