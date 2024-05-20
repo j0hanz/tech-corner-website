fbd5be1e31a01207733adef1456739693a5002ab
Linus Johansson <golfarenlj@gmail.com>
2024-05-20 18:06:31 +0000

    Added comments that explains the code

--- website/views.py
+++ website/views.py
@@ -7,12 +7,15 @@ from .forms import PostForm
 
 @login_required
 def create_post(request):
+    """
+    Handle the creation of a new post. Only accessible to logged-in users.
+    """
     if request.method == "POST":
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.save()
+            post.save()  # Save the Post object to the database
             messages.success(request, "You have uploaded a new Post!")
             return redirect("index")
         else:
@@ -26,10 +29,16 @@ def create_post(request):
 
 
 def index(request):
+    """
+    Display a list of published posts on Home page.
+    """
     posts = Post.objects.filter(status=1).order_by("-date")
     return render(request, "website/index.html", {"posts": posts})
 
 
 def post_detail(request, slug):
+    """
+    Display the details of a post.
+    """
     post = get_object_or_404(Post, slug=slug)
     return render(request, "website/post_detail.html", {"post": post})
