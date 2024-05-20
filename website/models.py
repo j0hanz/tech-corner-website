c6051e20364e98b341dc894c71931ff6dd6b0592
Linus Johansson <golfarenlj@gmail.com>
2024-05-20 18:05:42 +0000

    Removed banner model. Added comments that explains the code.

--- website/models.py
+++ website/models.py
@@ -5,30 +5,49 @@ from django.utils.text import slugify
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
+"""
+Define the status choices for the Post model
+"""
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True)
+    """
+    Define the Post model
+    """
+
+    title = models.CharField(max_length=200, unique=True)  # Title
+    slug = models.SlugField(max_length=200, unique=True, blank=True)  # Slug
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="user_posts"
-    )
-    banner = CloudinaryField("image", default="logo.webp", blank=True)
-    body = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=1)
-    excerpt = models.TextField(blank=True)
-    updated_on = models.DateTimeField(auto_now=True)
+    )  # Author
+    body = models.TextField()  # Body
+    date = models.DateTimeField(auto_now_add=True)  # Date
+    status = models.IntegerField(
+        choices=STATUS, default=1
+    )  # Status (Default "Published")
+    excerpt = models.TextField(
+        blank=True
+    )  # Excerpt (Only available in admin interface)
+    updated_on = models.DateTimeField(
+        auto_now=True
+    )  # Date the post was last updated
 
     class Meta:
-        ordering = ["-date"]
+        """
+        Meta options for the Post model
+        """
+
+        ordering = ["-date"]  # Orders posts by date in descending order
 
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
 
 def unique_slug(instance, new_slug=None):
+    """
+    Generate a unique slug for the post
+    """
     slug = slugify(instance.title) if new_slug is None else new_slug
     Klass = instance.__class__
     qs_exists = Klass.objects.filter(slug=slug).exists()
@@ -40,5 +59,8 @@ def unique_slug(instance, new_slug=None):
 
 @receiver(pre_save, sender=Post)
 def save_post_receiver(sender, instance, *args, **kwargs):
+    """
+    Signal to generate a slug before saving the Post
+    """
     if not instance.slug:
         instance.slug = unique_slug(instance)
