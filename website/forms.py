cbcc2fa416e554b89dd82f7e853fac6775dc921e
Linus Johansson <golfarenlj@gmail.com>
2024-05-21 12:05:12 +0000

    new form for edit post, only body can be edited

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Post
 
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'body']
+        fields = ["title", "body"]
+
+
+class EditPostBodyForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["body"]
