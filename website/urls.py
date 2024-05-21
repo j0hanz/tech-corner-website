1d153e9d2a94869a5dd2edd963a3ef1b50ff228c
Linus Johansson <golfarenlj@gmail.com>
2024-05-21 12:06:09 +0000

    url for edit post

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('edit-post/<int:post_id>/', views.edit_post, name='edit_post'),
     path('posts/<slug:slug>/', views.post_detail, name='post_detail'),
     path('create-post/', views.create_post, name='create_post'),
     path('about/', views.about, name='about'),
