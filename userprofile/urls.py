155b57423a833aa136612ae30748b288ebdc414c
Linus Johansson <golfarenlj@gmail.com>
2024-05-23 11:44:18 +0000

    delete comment url added

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('edit-profile/', views.edit_profile, name='edit_profile'),
     path('profile/', views.profile_page, name='profile_page'),
     path('delete-post/<int:post_id>/', views.delete_post, name='delete_post'),
+    path("delete-comment/<int:comment_id>/", views.delete_comment, name="delete_comment"),
     path('delete_account/', delete_account, name='delete_account'),
     path('user-posts/', views.user_posts, name='user_posts'),
     path('profile/<str:username>/', views.view_profile, name='view_profile'),
