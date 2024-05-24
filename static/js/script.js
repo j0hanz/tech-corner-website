671982b1c2cfa3de91cd79ee4bc85fcfc5015908
Linus Johansson <golfarenlj@gmail.com>
2024-05-24 09:56:09 +0000

    comment added for timeout function

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,14 @@
+// Set a timeout to execute the alert messages
 window.setTimeout(function () {
   const alert = document.querySelector(".alert");
   if (alert) {
+    // Remove the 'show' class from the alert element
     alert.classList.remove("show");
+    // Add the 'fade' class to the alert element to start the fade-out effect
     alert.classList.add("fade");
-    alert.addEventListener("transitionend", () => alert.remove());
+    alert.addEventListener("transitionend", () => {
+      // Remove the alert element from the DOM after the fade-out transition ends
+      alert.remove();
+    });
   }
-}, 5000); // 5 seconds
+}, 7000); // 7000 milliseconds = 7 seconds
