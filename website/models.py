29759321fad9bfb63a0a0810acb002e51bcd635c
Linus Johansson <golfarenlj@gmail.com>
2024-09-24 20:51:54 +0000

    Clean up imports, update code formatting, and remove null=True in CharField

--- website/models.py
+++ website/models.py
@@ -1,89 +1,82 @@
-from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
-from django.utils.text import slugify
+from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
-
-"""
-Define the status choices for the Post model
-"""
-STATUS = ((0, "Draft"), (1, "Published"))
+from django.utils.text import slugify
 
 
 class Post(models.Model):
-    """
-    Model for Post
-    """
+    """Model for Post."""
+
+    STATUS = ((0, 'Draft'), (1, 'Published'))
 
     title = models.CharField(max_length=50, unique=True)  # Title
     slug = models.SlugField(max_length=200, unique=True, blank=True)  # Slug
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="user_posts"
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_posts',
     )  # Author
     body = models.TextField()  # Body
     date = models.DateTimeField(auto_now_add=True)  # Date
     status = models.IntegerField(
-        choices=STATUS, default=1
+        choices=STATUS,
+        default=1,
     )  # Status (Default "Published")
     excerpt = models.TextField(
-        blank=True
+        blank=True,
     )  # Excerpt (Only available in admin interface)
     updated_on = models.DateTimeField(
-        auto_now=True
+        auto_now=True,
     )  # Date the post was last updated
 
     class Meta:
-        """
-        Meta options for the Post model
-        """
+        """Meta options for the Post model."""
 
-        ordering = ["-date"]  # Orders posts by date in descending order
+        ordering = ['-date']  # Orders posts by date in descending order
 
-    def __str__(self):
-        return f"{self.title} | written by {self.author}"
+    def __str__(self) -> str:
+        return f'{self.title} | written by {self.author}'
 
 
 class Comment(models.Model):
-    """
-    Model for Comments
-    """
+    """Model for Comments."""
 
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments"
+        Post,
+        on_delete=models.CASCADE,
+        related_name='comments',
     )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="comments"
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments',
     )
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ['created_on']
 
-    def __str__(self):
-        return f"Comment {self.body} by {self.author}"
+    def __str__(self) -> str:
+        return f'Comment {self.body} by {self.author}'
 
 
 def unique_slug(instance, new_slug=None):
-    """
-    Generate a unique slug for the post.
-    Credit:
-    https://fazlerabby7.github.io/auto-generating-unique-slug-django-generic-approach/
-    """
-    slug = slugify(instance.title) if new_slug is None else new_slug
+    """Generate a unique slug for the post."""
+    slug = new_slug or slugify(instance.title)
     Klass = instance.__class__
-    qs_exists = Klass.objects.filter(slug=slug).exists()
-    if qs_exists:
-        new_slug = f"{slug}-{Klass.objects.filter(slug=slug).count() + 1}"
+    qs = Klass.objects.filter(slug=slug).exists()
+    if qs:
+        new_slug = (
+            f'{slug}-{Klass.objects.filter(slug__startswith=slug).count() + 1}'
+        )
         return unique_slug(instance, new_slug=new_slug)
     return slug
 
 
 @receiver(pre_save, sender=Post)
-def save_post_receiver(sender, instance, *args, **kwargs):
-    """
-    Signal to generate a slug before saving the Post
-    """
+def save_post_receiver(sender, instance, *args, **kwargs) -> None:
+    """Signal to generate a slug before saving the Post."""
     if not instance.slug:
         instance.slug = unique_slug(instance)
