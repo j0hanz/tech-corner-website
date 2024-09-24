11c02b0e239b7e05a01a3e0c01cce6083f001e00
Linus Johansson <golfarenlj@gmail.com>
2024-09-24 20:51:34 +0000

    Refactor form fields and attributes

--- website/forms.py
+++ website/forms.py
@@ -1,70 +1,65 @@
 from django import forms
-from .models import Post, Comment
+
+from .models import Comment, Post
 
 
 class PostForm(forms.ModelForm):
-    """
-    Form for creating a new post.
-    """
+    """Form for creating a new post."""
 
     title = forms.CharField(
         widget=forms.TextInput(
-            attrs={"placeholder": "Enter the title here..."}
+            attrs={'placeholder': 'Enter the title here...'},
         ),
-        label="Title",
+        label='Title',
     )
     body = forms.CharField(
         widget=forms.Textarea(
             attrs={
-                "rows": 7,
-                "cols": 32,
-                "placeholder": "Write your post here...",
-            }
+                'rows': 7,
+                'cols': 32,
+                'placeholder': 'Write your post here...',
+            },
         ),
-        label="Body",
+        label='Body',
     )
 
     class Meta:
         model = Post
-        fields = ["title", "body"]
+        fields = ['title', 'body']
 
 
 class EditPostForm(forms.ModelForm):
-    """
-    Form for editing an existing post.
-    """
+    """Form for editing an existing post."""
 
     body = forms.CharField(
         widget=forms.Textarea(
             attrs={
-                "rows": 7,
-                "cols": 32,
-                "placeholder": "Edit your post here...",
-            }
+                'rows': 7,
+                'cols': 32,
+                'placeholder': 'Edit your post here...',
+            },
         ),
-        label="Body",
+        label='Body',
     )
 
     class Meta:
         model = Post
-        fields = ["body"]
+        fields = ['body']
 
 
 class CommentForm(forms.ModelForm):
-    """
-    Form for creating a new comment on a post.
-    """
+    """Form for creating a new comment on a post."""
 
     body = forms.CharField(
         widget=forms.Textarea(
             attrs={
-                "class": "comment-textarea",
-                "placeholder": "Write your comment here...",
-            }
+                'class': 'comment-textarea',
+                'placeholder': 'Write your comment here...',
+            },
         ),
-        label="Comment",
+        label='Comment',
     )
 
     class Meta:
         model = Comment
-        fields = ["body"]
+        fields = ['body']
