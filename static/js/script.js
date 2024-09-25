d9f255b37499e6a4883d968202a6cd515d6c584f
Linus Johansson <golfarenlj@gmail.com>
2024-09-25 10:07:24 +0000

    Refactor alert handling and image preview function

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,25 @@
 // Set a timeout to execute the alert messages
 window.setTimeout(function () {
-  const alert = document.querySelector(".alert");
+  const alert = document.querySelector('.alert');
   if (alert) {
-    // Remove the 'show' class from the alert element
-    alert.classList.remove("show");
+    alert.classList.remove('show');
     // Add the 'fade' class to the alert element to start the fade-out effect
-    alert.classList.add("fade");
-    alert.addEventListener("transitionend", () => {
-      // Remove the alert element from the DOM after the fade-out transition ends
+    alert.classList.add('fade');
+    alert.addEventListener('transitionend', () => {
       alert.remove();
     });
   }
 }, 7000); // 7000 milliseconds = 7 seconds
+
+// Function to preview the image selected by the user
+function previewImage(event) {
+  const output = document.querySelector('.image-wrapper img');
+  if (output) {
+    output.src = URL.createObjectURL(event.target.files[0]);
+  } else {
+    const newImg = document.createElement('img');
+    newImg.src = URL.createObjectURL(event.target.files[0]);
+    newImg.classList.add('img-fluid');
+    document.querySelector('.image-wrapper').appendChild(newImg);
+  }
+}
