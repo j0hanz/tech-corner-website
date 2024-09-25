53242c02fa79b6c6143b6c9d33dd02b5d325861b
Linus Johansson <golfarenlj@gmail.com>
2024-09-25 21:03:43 +0000

    Refactor image upload functionality and improve user experience

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,43 @@
 // Set a timeout to execute the alert messages
 window.setTimeout(function () {
-  const alert = document.querySelector('.alert');
-  if (alert) {
+  const alerts = document.querySelectorAll('.alert');
+  alerts.forEach((alert) => {
     alert.classList.remove('show');
-    // Add the 'fade' class to the alert element to start the fade-out effect
     alert.classList.add('fade');
+
+    // Remove the alert after the fade transition ends
     alert.addEventListener('transitionend', () => {
-      alert.remove();
+      if (alert.classList.contains('fade')) {
+        alert.remove();
+      }
     });
-  }
-}, 7000); // 7000 milliseconds = 7 seconds
+  });
+}, 7000);
 
 // Function to preview the image selected by the user
 function previewImage(event) {
-  const output = document.getElementById('preview-image');
-  const placeholderText = document.querySelector('.placeholder-text');
-  if (event.target.files && event.target.files[0]) {
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.classList.remove('upload-icon');
-    placeholderText.style.display = 'none';
+  const file = event.target.files[0];
+
+  if (file && file.type.startsWith('image/')) {
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const imageWrapper = document.querySelector('.image-wrapper');
+      let output = imageWrapper.querySelector('img');
+
+      // Create an img element if not already present
+      if (!output) {
+        output = document.createElement('img');
+        output.classList.add('img-fluid');
+        imageWrapper.appendChild(output);
+      }
+
+      // Set the src of the img element to the uploaded image's data URL
+      output.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  } else {
+    console.error('The selected file is not a valid image.');
   }
 }
